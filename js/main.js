316db06689afd4b410e1ffc0337b364be79dd292
= <=>
2024-08-11 19:22:16 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,29 +1,17 @@
 function loadImage(id, targetId) {
-    var el = document.getElementById(id);
-    var targetEl = targetId ? document.getElementById(targetId) : el;
-    var imageToLoad;
-    if (el.dataset.image) {
-        imageToLoad = el.dataset.image;
-    } else if (typeof el.currentSrc === 'undefined') {
-        imageToLoad = el.src;
-    } else {
-        imageToLoad = el.currentSrc;
-    }
+    const el = document.getElementById(id);
+    const targetEl = targetId ? document.getElementById(targetId) : el;
+    const imageToLoad = el.dataset.image || el.currentSrc || el.src;
+
     if (imageToLoad) {
-        var img = new Image();
+        const img = new Image();
         img.src = imageToLoad;
-        img.onload = function () {
+        img.onload = () => {
             targetEl.classList.add('is-loaded');
         };
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    loadImage('wallpaper');
-    loadImage('pictureImage', 'picture');
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    loadImage('wallpaper');
-    loadImage('pictureImage', 'picture');
+document.addEventListener('DOMContentLoaded', () => {
+    ['wallpaper', 'pictureImage'].forEach(id => loadImage(id, id === "pictureImage" ? "picture" : undefined))
 });
